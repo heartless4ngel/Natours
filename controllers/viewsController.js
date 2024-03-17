2cbb80341fca421182921c1c2f28a2d60f74ef48
heartless4ngel <mattialancellotta@hotmail.com>
2024-03-17 18:17:11 +0100

    added login screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,7 +13,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: "reviews",
     fields: "review rating user",
@@ -21,5 +21,11 @@ exports.getTour = catchAsync(async (req, res) => {
   res.status(200).render("tour", {
     title: tour.name,
     tour,
+  });
+});
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  res.status(200).render("login", {
+    title: "Log into your account",
   });
 });
